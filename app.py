90d66200742b7df77baea78e0bb43fda0930fb6a
horokos <44499944+horokos@users.noreply.github.com>
2021-11-23 12:58:12 +0100

    Add files via upload

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, request, render_template_string, get_flashed_messages
+
+app = Flask(__name__)
+
+app.config['Flag'] = 'ukryta flaga'
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    name = ""
+    if request.method == 'POST':
+        name = "Hello " + request.form['name']
+    return render_template_string('''
+            Hello, what's your name? 
+            <form action="{{ url_for('index') }}" method="post">
+                <input name="name">
+                <input type="submit" value="send">
+            </form>
+            %s  ''' % name)
+    # try:
+    #     return render_template_string('''
+    #     Hello, what's your name?
+    #     <form action="{{ url_for('index') }}" method="post">
+    #         <input name="name">
+    #         <input type="submit">
+    #     </form>
+    #     %s  ''' % name)
+    # except Exception:
+    #     return render_template_string('''
+    #     Hello, what's your name?
+    #     <form action="{{ url_for('index') }}" method="post">
+    #         <input name="name">
+    #         <input type="submit">
+    #     </form>''')
+
+if __name__ == "__main__":
+    app.run(debug=True)
