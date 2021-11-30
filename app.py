e0cfea1fba631316c95ce8d97eec1f71e11225c4
horokos <44499944+horokos@users.noreply.github.com>
2021-11-30 12:04:24 +0100

    Update app.py

--- app.py
+++ app.py
@@ -9,28 +9,30 @@ def index():
     name = ""
     if request.method == 'POST':
         name = "Hello " + request.form['name']
-    return render_template_string('''
-            Hello, what's your name? 
-            <form action="{{ url_for('index') }}" method="post">
-                <input name="name">
-                <input type="submit" value="send">
-            </form>
-            %s  ''' % name)
-    # try:
-    #     return render_template_string('''
-    #     Hello, what's your name?
-    #     <form action="{{ url_for('index') }}" method="post">
-    #         <input name="name">
-    #         <input type="submit">
-    #     </form>
-    #     %s  ''' % name)
-    # except Exception:
-    #     return render_template_string('''
-    #     Hello, what's your name?
-    #     <form action="{{ url_for('index') }}" method="post">
-    #         <input name="name">
-    #         <input type="submit">
-    #     </form>''')
+    try:
+         return render_template_string('''
+         Hello, what's your name?
+         <form action="{{ url_for('index') }}" method="post">
+             <input name="name" size=100>
+             <input type="submit">
+         </form>
+         %s  ''' % name)
+    except Exception:
+         return render_template_string('''
+         Hello, what's your name?
+         <form action="{{ url_for('index') }}" method="post">
+             <input name="name">
+             <input type="submit">
+         </form>
+         Coś poszło nie tak :(''')
+    #return render_template_string('''
+    #        Hello, what's your name? 
+    #        <form action="{{ url_for('index') }}" method="post">
+    #            <input name="name" size=100>
+    #            <input type="submit" value="send">
+    #        </form>
+    #        %s  ''' % name)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
